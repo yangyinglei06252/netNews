989f344d2f78d15e3894e8bde2c2209a21805ee9
杨英雷 <1297247207@qq.com>
2017-03-10 19:00:27 +0800

    网易新建项目

---
+++ netNews/netNews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  netNews
+//
+//  Created by yangyinglei on 2017/3/10.
+//  Copyright © 2017年 yangyinglei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ netNews/netNews/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  netNews
+//
+//  Created by yangyinglei on 2017/3/10.
+//  Copyright © 2017年 yangyinglei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
